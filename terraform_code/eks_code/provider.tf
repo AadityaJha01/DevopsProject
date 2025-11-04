8efc6f625f99d3f98f48269c4481187f24eb1f8b
AadityaJha01 <jhaa98676@gmail.com>
2025-11-05 03:48:02 +0530

    Upgrade EKS module dependencies

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.76.0"
+    }
+  }
+}
+
 locals {
   region = "us-east-1"
   name   = "skycast-weather-cluster"
