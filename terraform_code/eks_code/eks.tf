7ee8237db67796a4c230d81933fe43a02bef0463
AadityaJha01 <jhaa98676@gmail.com>
2025-11-05 05:26:50 +0530

    Finalize cleanup after EKS removal

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -26,7 +26,7 @@ module "eks" {
       max_size     = 4
       desired_size = 2
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
 
       tags = {
